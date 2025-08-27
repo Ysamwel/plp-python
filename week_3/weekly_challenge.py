58689834793a3a4af903687c77992c33fecc2f06
Ysamwel <yvonneatieno2000@gmail.com>
2025-08-28 00:40:24 +0300

    divisible by 10

--- week_3/weekly_challenge.py
+++ week_3/weekly_challenge.py
@@ -1,4 +1,5 @@
-
+# Define the function to accept two input parameters called base and exponent
+# Calculate the result of base to the power of exponent
 
 def large_power(base, exponent):
     return (base ** exponent) > 5000
